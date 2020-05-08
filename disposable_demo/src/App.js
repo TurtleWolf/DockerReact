36f264335f5fce9e624a50cddd60b8fe3a1675dd
TurtleWolfe <JonPohlner@gmail.com>
2020-05-08 15:49:38 -0400

    disposableScavenge

--- disposable_demo/src/App.js
+++ disposable_demo/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          https://hub.docker.com/TurtleWolfe/DockerReact
+          Learn React
         </a>
       </header>
     </div>
