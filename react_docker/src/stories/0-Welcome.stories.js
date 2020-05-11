8d84a3a795ca613b9753a80b28f553bdaf3cd66b
TurtleWolfe <JonPohlner@gmail.com>
2020-05-11 14:28:24 -0400

    added app as a component to the storybook

--- react_docker/src/stories/0-Welcome.stories.js
+++ react_docker/src/stories/0-Welcome.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { linkTo } from '@storybook/addon-links';
 import { Welcome } from '@storybook/react/demo';
+import App from '../App';
 
 export default {
   title: 'Welcome',
@@ -12,3 +13,5 @@ export const ToStorybook = () => <Welcome showApp={linkTo('Button')} />;
 ToStorybook.story = {
   name: 'to Storybook',
 };
+
+export const app = () => <App />;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
